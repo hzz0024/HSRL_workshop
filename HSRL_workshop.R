5c02f533f6f6c50bf3df481d77133b7c4431888b
Honggang Zhao <hzz0024@auburn.edu>
2022-09-22 23:23:11 -0400

    update

--- HSRL_workshop.R
+++ HSRL_workshop.R
@@ -1,7 +1,7 @@
 
-## Welcome to the 2022 Consortium SNP Array Data Analysis Workshop -- session for population genetic analyses  
+## Welcome to the 2022 Consortium SNP Array Data Analysis Workshop -- session for population structure analyses  
 
-# This repository includes scripts and data associated with the practical sessions of population genetic analyses
+# This repository includes scripts and data associated with the practical sessions of population structure analyses
 # Prepared by Honggang Zhao in Sep 2022, with helps from Matt Hare lab. 
 
 # R packages --------------------------------------------------------------
@@ -40,7 +40,7 @@ library(bigsnpr)
 # See https://github.com/hzz0024/HSRL_workshop for figure illustration
 setwd("~/Desktop/HSRL_workshop")
 
-## Part1: Handling SNP array data: VCF filtering and formatting
+## Part1: Handling SNP array data: VCF filtering and formatting -----------------
 # 
 # In this demo we will use a vcf file produced from SNP array and converted by Axiom Analysis Suite. 
 # The file includes 65,893 SNPs (i.e., 66K array markers) and 125 individuals from four populations. 
@@ -63,7 +63,7 @@ library(vcfR)
 #read in vcf as vcfR
 vcfR <- read.vcfR("./Example_data/example_66k_n125.recode.vcf")
 
-### Step 2: Quality filtering for missing data and minor allele counts ----------------------------------------------------------------------------------------------------------------------
+### Step 2: Quality filtering for missing data  ----------------------------------------------------------------------------------------------------------------------
 
 # Checking missing data by SNP and the effect of various cutoffs on the missingness of each sample
 missing_by_snp(vcfR)
@@ -74,14 +74,14 @@ vcfR_missing<-missing_by_snp(vcfR, cutoff = .95)
 # 1.66% of SNPs fell below a completeness cutoff of 0.95 and were removed from the VCF
 
 # Write out vcf files for downstream analyses.
-vcfR::write.vcf(vcf_missing_mac, "./example_66k_n125_missing95_mac6.vcf.gz")
+vcfR::write.vcf(vcf_missing_mac, "./example_66k_n125_missing95.vcf.gz")
 
-## Part2: Principal component analysis (PCA) 
+## Part2: Principal component analysis (PCA) ----------------
 library(SNPfiltR)
 library(vcfR)
 
 #read in vcf as vcfR
-vcfR <- read.vcfR("./Example_data/example_66k_n125_missing95_mac6_hwe_LD_clump.recode.vcf")
+vcfR <- read.vcfR("./Example_data/example_66k_n125_missing95_hwe_LD_clump.recode.vcf")
 
 #generate popmap file. Two column popmap with 'id' and 'pop'
 popmap<-data.frame(id=colnames(vcfR@gt)[2:length(colnames(vcfR@gt))],pop=substr(colnames(vcfR@gt)[2:length(colnames(vcfR@gt))], 1,4))
@@ -91,44 +91,14 @@ head(popmap)
 # Perform PCA analyasis
 assess_missing_data_pca(vcfR=vcfR, popmap = popmap, thresholds=NULL,clustering = FALSE)
 
-## Part3: Admixture analysis -----------------------------------------------
-
-# lead LEA package
-library(LEA)
-
-#change vcf to geno 
-LEA::vcf2geno("./Example_data/example_66k_n125_missing95_mac6_hwe_LD_clump.recode.vcf",
-              output.file = "example_66k_n125_missing95_mac6_hwe_LD_clump.geno")
-# - number of detected individuals: 125
-# - number of detected loci:    1000
-
-# modeling ancestry proportions for different K: from K=1 to K=10
-obj <- snmf("example_66k_n125_missing95_mac6_hwe_LD_clump.geno", K = 1:10, ploidy = 2,
-            entropy = T, CPU =4, project = "new")
-
-# Find the best K from cross-entropy
-plot(obj, col = "blue4", cex = 1.4, pch = 19) #---best is 6 here
-
-#choose the best LEA run
-best = which.min(cross.entropy(obj, K = 2))
-
-# Plot ancestry proportions across samples
-barchart(obj, K=2,run=best,border=T,space=0,
-         col=c("grey","orange"), lab=tab_pop$pop,
-         xlab = "Individuals", ylab = "Ancestry proportions (K=2)") -> bp
-
-axis(1, at = 1:length(bp$order), 
-     labels = popmap[bp$order, "id"], las = 3, 
-     cex.axis = .6)
-
-## Part4: Fst statistics ---------------------------------------------------
+## Part3: Fst statistics ---------------------------------------------------
 # load packages
 library(hierfstat)
 library(pheatmap)
 library(vcfR)
 
 # load vcf file and convert it to genind format
-vcf_file = "./Example_data/example_66k_n125_missing95_mac6_hwe_LD_clump.recode.vcf"
+vcf_file = "./Example_data/example_66k_n125_missing95_hwe_LD_clump.recode.vcf"
 vcf <- read.vcfR(vcf_file, verbose = FALSE)
 df <- vcfR2genind(vcf)
 df@pop <- factor(popmap$pop)
@@ -142,7 +112,7 @@ plot_dt
 # plot the Fst matrix with heatmap
 pheatmap(plot_dt, display_numbers = T, cellwidth=50, cellheight=40, main="Pairwise FST")
 
-## Part5: Genetic diversity (Heterozygosity and Allelic Richness) ----------
+## Part4: Genetic diversity (Heterozygosity and Allelic Richness) ----------
 
 # Calculate the allelic richness
 Arich <- allelic.richness(df,min.n=NULL,diploid=TRUE)
@@ -167,7 +137,35 @@ He
 #   LIW1    LIW2    NEH1    NEH2 
 # 0.31269 0.31558 0.30919 0.30327 
 
+## Part5: Admixture analysis -----------------------------------------------
 
+# lead LEA package
+library(LEA)
+
+#change vcf to geno 
+LEA::vcf2geno("./Example_data/example_66k_n125_missing95_hwe_LD_clump.recode.vcf",
+              output.file = "example_66k_n125_missing95_hwe_LD_clump.geno")
+# - number of detected individuals: 125
+# - number of detected loci:    1000
+
+# modeling ancestry proportions for different K: from K=1 to K=10
+obj <- snmf("example_66k_n125_missing95_hwe_LD_clump.geno", K = 1:10, ploidy = 2,
+            entropy = T, CPU =4, project = "new")
+
+# Find the best K from cross-entropy
+plot(obj, col = "blue4", cex = 1.4, pch = 19) #---best is 6 here
+
+#choose the best LEA run
+best = which.min(cross.entropy(obj, K = 2))
+
+# Plot ancestry proportions across samples
+barchart(obj, K=2,run=best,border=T,space=0,
+         col=c("grey","orange"), lab=tab_pop$pop,
+         xlab = "Individuals", ylab = "Ancestry proportions (K=2)") -> bp
+
+axis(1, at = 1:length(bp$order), 
+     labels = popmap[bp$order, "id"], las = 3, 
+     cex.axis = .6)
 
 
 
